ff1b88dabed89e3673708905d6b81132a768a254
Vahtee Boo <vahtee.boo@gmail.com>
2022-03-24 14:17:08 +0200

    Initial Commit

---
+++ MultipleSheetsSTBC/MultipleSheetsSTBCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleSheetsSTBCApp.swift
+//  MultipleSheetsSTBC
+//
+//  Created by Vahtee Boo on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MultipleSheetsSTBCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
